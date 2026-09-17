f7f9d5238efca84861c8411eb9f92f1fa7ba6741
Vadikk <balevvadim123@gmail.com>
2026-09-17 11:53:50 +0300

    fixing script and readme

--- lab01/cleanup.sh
+++ lab01/cleanup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-[ $# -lt 1 ] && { echo "Usage: $0 <dir> [ext...]"; exit 1; }
-[ -d "$1" ] || { echo "Error: '$1' not found"; exit 1; }
+[ $# -lt 1 ] && { echo "Usage: ${0} <dir> [ext...]"; exit 1; }
+[ -d "${1}" ] || { echo "Error: '${1}' not found"; exit 1; }
 
-dir=$1; shift
+dir=${1}; shift
 [ $# -eq 0 ] && set -- tmp
 
 count=0
 for ext; do
-    n=$(find "$dir" -type f -name "*.${ext#.}" -print -delete | wc -l)
+    n=$(find "${dir}" -type f -name "*.${ext#.}" -print -delete | wc -l)
     ((count+=n))
 done
-echo "Deleted: $count"
+echo "Deleted: ${count}"
